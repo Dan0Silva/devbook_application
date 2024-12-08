c577a0272d4680ce12b0f1afcd8fc11ddec11d35
Dan0Silva <danilo.official22@hotmail.com>
2024-12-07 23:41:09 -0300

    creating a userModel and set the user table configuration

---
+++ devbook_backend/src/models/userModel.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	Id        uuid.UUID `json:"id,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	Nick      string    `json:"nick,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	Password  string    `json:"password,omitempty"`
+	CreatedAt time.Time `json:"createdAt,omitempty"`
+}
